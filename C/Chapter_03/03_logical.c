0d3ea999f60ebd2238de2aefc79654c868bc7463
Aayush Pd <aaayushpd@gmail.com>
2026-07-30 21:56:30 +0530

    Day 2 of learning

--- C/Chapter_03/03_logical.c
+++ C/Chapter_03/03_logical.c
@@ -4,6 +4,7 @@
         int a=0; int b=12;
         printf("The value of a and b is %d\n", a&&b);
         printf("The value of a or b is %d\n", a||b);
+        printf("The value of not(a) is %d\n", !a);
 
         return 0;
     }
