403a07bad5febfc7efc5f5b67586579c7844228d
Aayush Pd <aaayushpd@gmail.com>
2026-07-31 12:46:34 +0530

    Day 3 of learning

---
+++ C/Chapter_03/05_operator.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+    int a = 5;
+    int b = 6;
+    if(a=9){
+        printf("delhi");
+    }
+    else{
+        printf("jaipur");
+    }
+
+    printf("%d", a);
+}
