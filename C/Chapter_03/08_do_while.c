586e87b968b48195c16abf981113df4eadca812e
Aayush Pd <aayushkumar92710156@gmail.com>
2026-08-10 20:42:19 +0530

    Day 10 of learning (1)

---
+++ C/Chapter_03/08_do_while.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+    int main(){
+        int i= 12;
+        int b= 5;
+
+        for(int i=5; i; i--){
+            printf("%d \n", i);
+        }
+
+        for(int b=6; b; b--){
+            printf("%d\n\n", b);
+        }
+    }
