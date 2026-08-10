c3d3fbfb4c89d5ec5a48b64b2dfb2b1134146f26
Aayush Pd <aayushkumar92710156@gmail.com>
2026-08-10 18:42:05 +0530

    Day 10 of learning

---
+++ C/College/FIRST.C
@@ -0,0 +1,16 @@
+void main()
+  {
+    int a;
+    int b;
+    int c;
+    clrscr();
+
+    printf("Enter the value of a: ");
+    scanf("%d", &a);
+
+    printf("Enter the value of b: ");
+    scanf("%d", &b);
+
+    c= a+b;
+    printf("The sum of %d & %d is: %d", a, b, c);
+  }
