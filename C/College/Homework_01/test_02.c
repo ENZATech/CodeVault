d2790e1cd7091b7db41f1187379c4f1289a8a5d6
Aayush Pd <aayushkumar92710156@gmail.com>
2026-08-11 11:13:37 +0530

    Day 11 of learning

---
+++ C/College/Homework_01/test_02.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+    void main(){
+        int a = 5;
+        int b = 9;
+
+        if(a++ == 5 && ++b !=9)
+        printf("Indra");
+
+        else
+        printf("India");
+
+        printf("%d, %d", a, b);
+
+    }
+    
