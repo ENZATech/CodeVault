82eab71096595c9843e5fb99f6812abeab7e76de
Aayush Pd <aayushkumar92710156@gmail.com>
2026-08-16 13:40:01 +0530

    Day 16 of learning

---
+++ C/Chapter_05/01_function.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+    //Function Prototype.
+    int sum(int, int);
+
+    //Function definition.
+    int sum(int x, int y){
+        //printf("The sum is %d", x+y);
+
+        return x+y;
+    }
+
+    int main(){
+        int x=21;
+        int y=32;
+
+        //int c = a+b;
+        //printf("The sum is %d", c);
+        int c = sum(x,y);
+        printf("%d \n", c);
+
+        int a1 = 456;
+        int b1 = 1234;
+
+        int c1 = sum(a1, b1);
+        printf("%d", c1);
+
+        return 0;
+    }
